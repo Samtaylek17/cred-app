fffa57396b022e4d8369b0da075c443b6ffce4c3
Laketu Adeleke <sodlak007@gmail.com>
2018-12-04 14:38:03 +0100

    dashboard implementation

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(function() {
+		$('.acct-match').matchHeight();
+	});
+
+	$(function(){
+	  $('.bxslider').bxSlider({
+	    mode: 'horizontal',
+	    captions: true,
+	    slideWidth: 600
+	  });
+	});
+});
